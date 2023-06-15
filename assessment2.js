f82420b8ca63839911ac8d13e7f6616539222a15
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-06-15 08:25:42 +0300

    assessement2 answers

---
+++ assessment2.js
@@ -0,0 +1,92 @@
+// **African Cuisine:** You're creating a recipe app specifically for African cuisine.
+// The app needs to handle recipes from different African countries, each with its
+// unique ingredients, preparation time, cooking method, and nutritional
+// information. Consider creating a `Recipe` class, and think about how you might
+// create subclasses for different types of recipes (e.g., `MoroccanRecipe`,
+// `EthiopianRecipe`, `NigerianRecipe`), each with their own unique properties and
+// methods.
+
+
+// input
+// Recipes
+// output
+// Different types of recipes
+// Process
+// Creating a class known as Recipe ans pass in the attributes
+// create sublasses and pass in unique properties
+
+
+class Recipe{
+    constructor(uniqiueIngredients,preparationTime,cookingMethod,NutritionalInformation){
+        this.uniqiueIngredients = uniqiueIngredients;
+        this.preparationTime = preparationTime,
+        this.cookingMethod= cookingMethod,
+        this.NutritionalInformation=NutritionalInformation
+    }
+}
+    class MoroccanRecipe extends Recipe{
+        const = RecipeIsAvailable ()
+
+        }
+    }
+
+    // **Ancestral Stories:** In many African cultures, the art of storytelling is passed
+    // down from generation to generation. Imagine you're creating an application that
+    // records these oral stories and translates them into different languages. The
+    // stories vary by length, moral lessons, and the age group they are intended for.
+    // Think about how you would model `Story`, `StoryTeller`, and `Translator`
+    // objects, and how inheritance might come into play if there are different types of
+    // stories or storytellers.
+
+//   Input
+
+//   Ancestral Stories
+//   output
+
+//   application that will record the Stories
+//   translate the stories into different languages
+//   Process
+//   creating a class and pass in the attributesmp
+//   create methods(record the stories,translating to different languages)
+class AncestralStories{
+    constructor(length,moralLessons,ageGroup){
+        this.length = length,
+        this.moralLessons = moralLessons,
+        this.ageGroup= ageGroup
+    }
+   
+}
+   let = recordStories extends AncestralStories(length,moralLessons,ageGroup){
+
+}
+
+// **Wildlife Preservation:** You're a wildlife conservationist working on a
+// program to track different species in a national park. Each species has its own
+// characteristics and behaviors, such as its diet, typical lifespan, migration
+// patterns, etc. Some species might be predators, others prey. You'll need to create classes 
+// to model `Species`, `Predator`, `Prey`, etc., and think about how
+// these classes might relate to each other through inheritance.
+
+// input
+// wildlifePreservation
+// output
+// tracking different species
+// process
+// creating  class and pass in the attributes
+// creating subclasses 
+// creating a method
+
+
+class Species{
+    constructor(diet,typicalLifespan,migrationPatterns){
+        this.diet=diet,
+        this.typicalLifespan= typicalLifespan,
+        this.migrationPatterns= migrationPatterns
+
+    }
+}
+class predators{
+
+}
+clas
+
