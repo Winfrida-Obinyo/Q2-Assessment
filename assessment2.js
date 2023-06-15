6c15dee56b11f890b36366c4af9108a937842d54
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-06-15 09:17:01 +0300

    assessement2 answers

--- assessment2.js
+++ assessment2.js
@@ -85,8 +85,123 @@ class Species{
 
     }
 }
-class predators{
+class Predators{
 
 }
-clas
+class Prey{
 
+}
+
+// **African Music Festival:** You're in charge of organizing a Pan-African music
+// festival. Many artists from different countries, each with their own musical style
+// and instruments, are scheduled to perform. You need to write a program to
+// manage the festival lineup, schedule, and stage arrangements. Think about how
+// you might model the `Artist`, `Performance`, and `Stage` classes, and consider
+// how you might use inheritance if there are different types of performances or
+// stages.
+
+// INPUT
+// Music Festival
+// OUTPUT
+// Program that manages music festive
+// Process
+// create a class and pass in the attributes
+// create a subclasses
+// create methods
+
+class MusicFestivals{
+    constructor(festivalLineup,schedule,stageArrangements){
+        this.festivalLineup =festivalLineup
+        this.schedule = schedule
+        this.stageArrangements = stageArrangements
+
+    }
+}
+
+
+
+
+
+
+
+
+// Create a class called Product with attributes for name, price, and quantity.
+// Implement a method to calculate the total value of the product (price * quantity).
+// Create multiple objects of the Product class and calculate their total values.
+
+// input
+// product
+// output
+// total value ot the product
+// process
+// create a class and pass the attributes
+// create method to calculte the total value of the product 
+// create objects of the class product and caete the total values
+
+class Product{
+    constructor(price,quantity){
+        this.price = price
+        this.quantity = quantity
+
+    }
+}
+
+
+
+// Implement a class called Student with attributes for name, age, and grades (a
+//     list of integers). Include methods to calculate the average grade, display the
+//     student information, and determine if the student has passed (average grade >=
+//     60). Create objects for the Student class and demonstrate the usage of these
+//     methods.
+
+// input
+// Student
+// output
+// process
+// create a class and pass in the attributes
+// create methods
+// create object of class student that will demontrate the usage of the methods
+
+class Student{
+    constructor(name,age,grades,){
+        this.name = name,
+        this.age = age,
+        this.grades = grades
+
+    }
+
+}
+
+
+
+// Create a FlightBooking class that represents a flight booking system. Implement
+// methods to search for available flights based on destination and date, reserve
+// seats for customers, manage passenger information, and generate booking
+// confirmations.
+
+
+// input
+// FlightBooking
+// output
+// Available flights
+// process
+// create a class that represents the booking system
+// create methods for available flights
+
+class FlightBooking{
+    constructor(destination,date,reserveSeats,passengerInformation,bookingConfirmations){
+        this.destination = destination
+        this.date= date
+        this.reserveSeats = reserveSeats
+        this.passengerInformation = passengerInformation
+        this.bookingConfirmations = bookingConfirmations
+
+    }
+}
+
+
+
+
+// Create a LibraryCatalog class that handles the cataloging and management of
+// books in a library. Implement methods to add new books, search for books by
+// title or author, keep track of available copies, and display book details.
