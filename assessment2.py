f636471fd259da7d338932deaf887370410480dd
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-06-15 08:28:49 +0300

    added assessment2.py

---
+++ assessment2.py
@@ -0,0 +1,29 @@
+#  **African Cuisine:** You're creating a recipe app specifically for African cuisine.
+#  The app needs to handle recipes from different African countries, each with its
+#  unique ingredients, preparation time, cooking method, and nutritional
+#  information. Consider creating a `Recipe` class, and think about how you might
+#  create subclasses for different types of recipes (e.g., `MoroccanRecipe`,
+#  `EthiopianRecipe`, `NigerianRecipe`), each with their own unique properties and
+#  methods.
+
+
+# input
+#  Recipes
+# output
+#  Different types of recipes
+#  Process
+#  Creating a class known as Recipe ans pass in the attributes
+# create sublasses and pass in unique properties
+
+
+class Recipe:
+    def __init__(self,unique_ingredients,preparation_time,cooking_method,Nutritional_information):
+        self.unique_ingredients = unique_ingredients,
+        self.preparation_time = preparation_time,
+        self.cooking_method= cooking_method,
+        self.Nutritional_information=Nutritional_information
+        
+class MoroccanRecipe:
+    def african_cuisine(self,):
+        if self.unique_ingredients
+
